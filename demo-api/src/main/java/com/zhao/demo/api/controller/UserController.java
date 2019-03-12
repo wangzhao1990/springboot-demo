a01fc0933354609793b7045bb6316911991c2542
wangzhao <wangzhao678@outlook.com>
2019-03-12 14:56:46 +0800

    swagger配置修改

--- demo-api/src/main/java/com/zhao/demo/api/controller/UserController.java
+++ demo-api/src/main/java/com/zhao/demo/api/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/demo-api/user")
+@RequestMapping("/user")
 @Api("UserController相关的api")
 public class UserController {
 
